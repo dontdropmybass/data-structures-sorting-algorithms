0fe1e768d59b52eb00a81f3bebfad7438583dc77
w0290029 <w0290029@nscc.ca>
2017-03-24 09:12:11 -0300

    Added final comments

--- data-structures-sorting-algorithms/Sorts.cpp
+++ data-structures-sorting-algorithms/Sorts.cpp
@@ -6,6 +6,7 @@ void Sorts::bubbleSort(int* arr, int size) {
     int j = 0;
     int tmp;
     while (swapped) {
+		//conditional set to false
         swapped = false;
         j++;
         for (int i = 0; i < size - j; i++) {
@@ -13,6 +14,7 @@ void Sorts::bubbleSort(int* arr, int size) {
                 tmp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = tmp;
+				//conditional set true will only be hit after sorting numbers
                 swapped = true;
             }
         }
@@ -25,13 +27,17 @@ void Sorts::bubbleSort(int* arr, int size) {
 
 void Sorts::selectionSort(int* arr, int size) {
     int outer, inner, min;
+
     for (outer = 0; outer < size - 1; outer++) {
+		//go through array at first index
         min = outer;
         for (inner = outer + 1; inner < size; inner++) {
+			//go through rest of array after the index and see if value is less then outer index
             if (arr[inner] < arr[min]) {
                 min = inner;
             }
         }
+		//overwriting the index with the lowest value found
         int temp = arr[outer];
         arr[outer] = arr[min];
         arr[min] = temp;
@@ -47,12 +53,15 @@ void Sorts::insertionSort(int* arr, int length)
 	int i, j, tmp;
 	for (i = 1; i < length; i++)
 	{
+		//move forwards through the array
 		j = i;
 		while (j > 0 && arr[j - 1] > arr[j])
 		{
+			//if the index of j - 1 is less then J swap them
 			tmp = arr[j];
 			arr[j] = arr[j - 1];
 			arr[j - 1] = tmp;
+			//continue to move backwards through the array comparing values
 			j--;
 		} //end of while loop
 	} //end of for loop
@@ -64,11 +73,15 @@ void Sorts::insertionSort(int* arr, int length)
 
 void Sorts::shellSort(int* arr, int length){
 int i, j, increment, temp;
+//get a gap "increment" initialized as half the size of the array,
 for (increment = length / 2; increment > 0; increment /= 2)
 {
+	//set up i to start at the increment value
+	//i's value increases with each iteration
 	for (i = increment; i<length; i++)
 	{
 		temp = arr[i];
+		//check the value at the index of J
 		for (j = i; j >= increment; j -= increment)
 		{
 			if (temp < arr[j - increment])
@@ -100,10 +113,11 @@ void Sorts::quickSort(int* arr, int left, int right) {
 	//pivot is set to the middle variable
 	int pivot = arr[(left + right) / 2];
 
-	//walk through the array
+	//walk through the array until value is smaller then pivot
 	while (i <= j) {
 		while (arr[i] < pivot)
 			i++;
+		//walk through array until value is larger then pivot
 		while (arr[j] > pivot)
 			j--;
 		//swap the values of I and J if I is less then or equal to J
@@ -116,10 +130,11 @@ void Sorts::quickSort(int* arr, int left, int right) {
 		}
 	};
 
-	//recursively call function until I is at the last index and J is at the first
+	//recall function if left is smaller then J with J as the new Right Value
 	if (left < j) {
 		quickSort(arr, left, j);
 	}
+	//recall function if Right is smaller then I with I as the new Left Value
 	if (i < right) {
 		quickSort(arr, i, right);
 	}
